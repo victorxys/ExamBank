171f1c088f8a7ed57b289896df1d7c077b23dd66
victorxys <victor.xys@gmail.com>
2025-03-11 10:25:09 +0800

    取消helmet引用

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       'test-school.mengyimengsao.top',
       'localhost',        // 允许 localhost 访问
       '127.0.0.1',       //允许127.0.0.1访问
-      // '192.168.1.104', // 如果需要通过内网 IP 访问，取消注释此行并替换为你的实际内网 IP
+      '192.168.1.105', // 如果需要通过内网 IP 访问，取消注释此行并替换为你的实际内网 IP
     ],
   }
 })
