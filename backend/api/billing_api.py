fea1ea383d2ceb55041f3673d239c97c1f83c194
victorxys <victor.xys@gmail.com>
2025-08-26 16:59:18 +0800

    修复账单列表拼音检索功能

--- backend/api/billing_api.py
+++ backend/api/billing_api.py
@@ -685,8 +685,11 @@ def get_bills():
             query = query.filter(
                 db.or_(
                     contract_poly.customer_name.ilike(f"%{search_term}%"),
+                    contract_poly.customer_name_pinyin.ilike(f"%{search_term}%"),
                     User.username.ilike(f"%{search_term}%"),
+                    User.name_pinyin.ilike(f"%{search_term}%"),
                     ServicePersonnel.name.ilike(f"%{search_term}%"),
+                    ServicePersonnel.name_pinyin.ilike(f"%{search_term}%"),
                 )
             )
 
